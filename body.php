91461f55bbad47221793a810a4bfe6e54adf4e22
UdoChudo <vla30@bk.ru>
2021-05-16 04:49:39 +0500

    merge old and new site

--- body.php
+++ body.php
@@ -1,5 +1,13 @@
+<body>
+    <h1>Test text</h1>
 
-    <body>
-        <h1>Test text</h1>
 
-    </body>
+    <form name="upload" action="handlers/download_skins.php" method="POST" ENCTYPE="multipart/form-data">
+        Введите ранее зарегистрированный никнейм:
+        <input type="text" name="nickname"> <br> Выберите файл скина для загрузки:
+        <input type="file" name="userfile"> <br>
+        <input type="submit" name="upload" value="Загрузить">
+    </form>
+
+
+</body>
