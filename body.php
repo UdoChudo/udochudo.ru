c714508de750aedef986088adc2c7afa3d57162f
UdoChudo <vla30@bk.ru>
2021-05-16 04:51:07 +0500

    fix name of script

--- body.php
+++ body.php
@@ -2,7 +2,7 @@
     <h1>Test text</h1>
 
 
-    <form name="upload" action="handlers/download_skins.php" method="POST" ENCTYPE="multipart/form-data">
+    <form name="upload" action="handlers/upload_skins.php" method="POST" ENCTYPE="multipart/form-data">
         Введите ранее зарегистрированный никнейм:
         <input type="text" name="nickname"> <br> Выберите файл скина для загрузки:
         <input type="file" name="userfile"> <br>
